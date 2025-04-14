76b6882cb9ff6c8a7ecc2bdef77ef3f54ca26223
0;47;167 <1274557116@qq.com>
2025-04-14 21:43:32 +0800

    hw08

---
+++ hw/hw08/test.scm
@@ -0,0 +1,9 @@
+(define (interleave lst1 lst2) 
+    (
+        cond((null? lst1) lst2)
+            ((null? lst2) lst1)
+            (else (cons (car lst1) (interleave lst2 (cdr lst1))))
+    )
+)
+
+(interleave '(1 2 3) '(4 5 6))
